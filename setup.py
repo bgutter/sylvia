43fbea3373d2538ae5b6b43b637d5a61bf2ea255
Brandon Guttersohn <bguttersohn@gmail.com>
2017-03-26 20:50:15 -0500

    Prepare for PyPI
    Add files needed to publish to PyPI.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'sylvia',
+  packages = ['sylvia'],
+  version = '0.1',
+  description = 'A tool for searching pronunciations in the CMU Pronouncing Dictionary with a regular-expression-like syntax.',
+  author = 'Brandon Guttersohn',
+  author_email = 'bguttersohn@gmail.com',
+  url = 'https://github.com/bgutter/sylvia',
+  download_url = 'https://github.com/bgutter/sylvia/archive/0.1.tar.gz',
+  keywords = [ 'cmudict', 'phoneme', 'phonetic', 'rhyme', 'regex' ],
+  classifiers = [],
+)
