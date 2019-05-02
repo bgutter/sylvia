b33fcfe3752caeb61a83eb04c3b8399b7c44c9a4
Brandon Guttersohn <bguttersohn@gmail.com>
2019-05-02 16:10:49 -0500

    Add meaningful error for runnign with Python3
    If we detect a Python3 interpreter at module init, tell the user to
    use Python 2. And be sure to apologize because it's 2019...

--- sylvia/__init__.py
+++ sylvia/__init__.py
@@ -1,3 +1,7 @@
+import sys
+if sys.version_info[0] > 2:
+    raise Exception( "Sorry, we're still on Python 2. Version 1.0 of Sylvia will finally move to Python 3." )
+
 from PhonemeDetails import *
 from LetterDetails import *
 from PronunciationInferencer import *
