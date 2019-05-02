1dcd5458c62dfd3ce338cf17e68f457e1b1d7937
Brandon Guttersohn <bguttersohn@gmail.com>
2019-05-02 16:13:07 -0500

    [release] Version 0.2
    This version completely reworks the CLI, and adds support for an EPC
    server via the "-e" switch. An Emacs major-mode for working with
    Sylvia is now maintained within the working tree.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'sylvia',
   packages = ['sylvia'],
-  version = '0.1',
+  version = '0.2',
   description = 'A tool for searching pronunciations in the CMU Pronouncing Dictionary with a regular-expression-like syntax.',
   author = 'Brandon Guttersohn',
   author_email = 'bguttersohn@gmail.com',
   url = 'https://github.com/bgutter/sylvia',
-  download_url = 'https://github.com/bgutter/sylvia/archive/0.1.tar.gz',
+  download_url = 'https://github.com/bgutter/sylvia/archive/0.2.tar.gz',
   keywords = [ 'cmudict', 'phoneme', 'phonetic', 'rhyme', 'regex' ],
   classifiers = [],
   package_data={'': ['cmudict.sylviabin']}
